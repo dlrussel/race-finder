750831ce5d19a2d630f65bcc3998db4477aebdeb
Diane Russel <dianerussel@rogers.com>
2024-06-19 17:56:46 -0400

    Move raceDetails to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RaceDetails from "./components/RaceDetails";
+import RaceDetails from "./pages/RaceDetails";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home";
 
