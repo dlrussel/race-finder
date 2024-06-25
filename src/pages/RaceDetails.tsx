bdebd6de31917a780209d700137041b834bbb498
Diane Russel <dianerussel@rogers.com>
2024-06-24 21:40:04 -0400

    race details page

--- src/pages/RaceDetails.tsx
+++ src/pages/RaceDetails.tsx
@@ -1,7 +1,8 @@
-import { Spinner, Text } from "@chakra-ui/react";
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import fetchRaceDetails from "../fetchRaceDetails";
+import formatDate from "../utilities/formatDate";
 
 export default function RaceDetails() {
   const { id } = useParams();
@@ -14,12 +15,13 @@ export default function RaceDetails() {
   const race = results?.data?.race;
 
   return (
-    <>
-      <h2>{race.name}</h2>
-      <Text>{`${race.city}, ${race.state}, ${race.country}`}</Text>
-      <a href={race.registration_link} target="_blank">
+    <Box px={4}>
+      <Heading as="h2">{race.name}</Heading>
+      <Text>Date: {formatDate(race.date)}</Text>
+      <Text mb={3}>{`${race.city}, ${race.state}, ${race.country}`}</Text>
+      <a className="button" href={race.registration_link} target="_blank">
         Register now
       </a>
-    </>
+    </Box>
   );
 }
